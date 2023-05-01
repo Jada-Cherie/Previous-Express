1eddf97c022db7e72a82ef2314ca7d1c8e2ffd79
Jada-Cherie <jada.c.townsend@gmail.com>
2023-05-01 15:53:29 -0400

    recommit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+
+const url = "mongodb+srv://demo:demo@cluster0.4jwahmr.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "demo";
+
+app.listen(3000, () => {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').insertOne({name: req.body.name, thumbUp: ''}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/messages', (req, res) => {
+  const inputString = req.body.name.toLowerCase();
+  const isPalindrome = inputString === inputString.split('').reverse().join('');
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name}, {
+    $set: {
+      thumbUp: isPalindrome ? 'Yes it is a Palindrome' : 'No it is not a Palindrome'
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/messages', (req, res) => {
+  db.collection('messages').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
